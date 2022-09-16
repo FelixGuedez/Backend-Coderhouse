143c7094ef81252d7a5ac0dbce3b5c4b3f3542ad
Felix Guedez <felixgue86@gmail.com>
2022-09-16 19:45:07 -0300

    primera base de datos

--- desafio-7-Base-de-Datos/src/batch/createTables.js
+++ desafio-7-Base-de-Datos/src/batch/createTables.js
@@ -6,23 +6,19 @@ export const knexCliMens = knex(config.dbMens);
 
 export async function CrearTablas() {
 
-    // knexCliProd.schema.dropTableIfExists('productos')
-    //     .then(() => {
-    //         knexCliProd.schema.createTable('productos', table => {
-    //             table.increments('id').primary();
-    //             table.string('nombre', 50).notNullable();
-    //             table.string('precio', 50).notNullable();
-    //             table.string('imagen', 50).notNullable();
-    //         })
-    //             .then(() => console.log("Tabla creada"))
-    //             .catch(err => {
-    //                 console.log(err);
-    //                 throw err;
-    //             })
-    //             .finally(() => {
-    //                 knexCliProd.destroy();
-    //             });
-    //     });
+    const existProd = knexCliMens.schema.hasTable('productos')
+    if (!existProd) {
+        await
+            knexCliProd.schema.createTable('productos', table => {
+                table.increments('id').primary();
+                table.string('nombre', 50).notNullable();
+                table.string('precio', 50).notNullable();
+                table.string('imagen', 50).notNullable();
+            });
+            console.log("Tabla de Productos creada")
+        } else {
+            console.log('Tabla de Productos Existente')
+        }
 
     const existMens = knexCliMens.schema.hasTable('mensajes')
     if (!existMens) {
@@ -33,17 +29,9 @@ export async function CrearTablas() {
                 table.string('mensaje', 50).notNullable();
                 table.string('fecha', 50).notNullable();
             })
-        console.log("Tabla creada")
-        // .then(() => console.log("Tabla creada"))
-        // .catch(err => {
-        //     console.log(err);
-        //     throw err;
-        // })
-        // // .finally(() => {
-        // //     knexCliMens.destroy();
-        // // });
+        console.log("Tabla de mensajes creada")
     } else {
-        console.log('Tabla ya creada anteriormente')
+        console.log('Tabla de mensajes Existente')
     }
 }
 
